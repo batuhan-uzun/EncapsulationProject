acdc032cc41418e028ac250243bbb1254c8e8e5a
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-04 04:59:50 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationProject
+{
+    internal class Car
+    {
+        public string Brand { get; set; } // Assigned brand, model, color and number of doors properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        private int _numberOfDoors; // Created a variable with private int for encapsulation
+        public int NumberOfDoors
+        {
+            get { return _numberOfDoors; } 
+            set
+            {
+                if (value == 2 || value == 4) // Logical control for the number of doors to be 2 or 4
+                    _numberOfDoors = value;
+                else
+                {
+                    _numberOfDoors = -1;
+                    Console.WriteLine("Kapı sayısı 2 veya 4 olabilir!");
+                }
+                    
+            }
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,11 @@
+﻿using EncapsulationProject;
+
+Car car1 = new Car(); // Created car1 object using car class
+Console.WriteLine("Arabanın markasını giriniz");
+car1.Brand = Console.ReadLine();
+Console.WriteLine("Arabanın modelini giriniz");
+car1.Model = Console.ReadLine();
+Console.WriteLine("Arabanın rengini giriniz");
+car1.Color = Console.ReadLine();
+Console.WriteLine("Arabanın kapı sayısını giriniz");
+car1.NumberOfDoors = Convert.ToInt32(Console.ReadLine());
